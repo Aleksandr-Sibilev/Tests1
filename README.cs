2cfe4a38d78cfb0685dbdcbbe0349188775d2e79
Aleksandr-Sibilev <114755514+Aleksandr-Sibilev@users.noreply.github.com>
2022-09-30 17:53:31 +0300

    Update README.cs

--- README.cs
+++ README.cs
@@ -2,34 +2,34 @@
 
 //Task_19
 
-// Console.Clear();
+Console.Clear();
+
+void CheckPalindromicNumber(int number)
+{
+    if(number >= 10000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
  
-// void CheckPalindromicNumber(int number)
-// {
-//     if(number >= 10000)
-//     {
-//         int division1 = number / 10000;
-//         int remainder1 = number % 10;
+            if(division1 == remainder1)
+            {
+                number = number / 10;
+                int division2 = (number / 100) % 10;
+                int remainder2 = number % 10;
+                if(division2 == remainder2)
+                    Console.WriteLine("Да");
+            }
+            else 
+            Console.WriteLine("Нет");
+         
+    }
+    else
+    Console.WriteLine("Некорректное число!");
+}
  
-//             if(division1 == remainder1)
-//             {
-//                 number = number / 10;
-//                 int division2 = (number / 100) % 10;
-//                 int remainder2 = number % 10;
-//                 if(division2 == remainder2)
-//                     Console.WriteLine("Да");
-//             }
-//             else 
-//             Console.WriteLine("Нет");
-            
-//     }
-//     else
-//     Console.WriteLine("Некорректное число!");
-// }
- 
-// Console.WriteLine("Введите пятизначное число:");
-// int number = int.Parse(Console.ReadLine()!);
-// CheckPalindromicNumber(number);
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
 
 //Task_21
 
